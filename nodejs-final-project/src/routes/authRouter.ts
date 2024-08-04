5d679c04f4819e896d6b47b75042f9f6ddaa7e35
majolivero <majolivero28@gmail.com>
2024-08-04 15:44:45 -0500

    comentarios

--- nodejs-final-project/src/routes/authRouter.ts
+++ nodejs-final-project/src/routes/authRouter.ts
@@ -1,6 +1,9 @@
 import { Router } from 'express';
-import AuthController from '../controllers/authController';
+import AuthController from '../controllers/authController'; //importa el AuthController que contiene la lógica de autenticación
 
-export const authRouter = Router();
+export const authRouter = Router(); //Crea una instancia de Router llamada authRouter
 
-authRouter.post('/login', AuthController.login);
+authRouter.post('/login', AuthController.login); //Define una ruta POST en login que etsa vinculada al método login del AuthController
+
+//Esta ruta se utiliza para manejar solicitudes de inicio de sesión, donde los datos de la solitud POST 
+//por ejemplo, nombre de usuario y contraseña, son procesados por el método login del AuthController
--- nodejs-final-project/src/routes/productRoutes.ts
+++ nodejs-final-project/src/routes/productRoutes.ts
@@ -3,7 +3,9 @@ import ProductController from '../controllers/productController';
 
 export const productRouter = Router();
 
-productRouter.get('/', ProductController.getAllProducts);
-productRouter.get('/:id', ProductController.getProductById);
-productRouter.get('/user/:userId', ProductController.getProductsByUserId);
-productRouter.post('/', ProductController.createProduct);
+productRouter.get('/', ProductController.getAllProducts); //Obtener todos los productos
+productRouter.get('/:id', ProductController.getProductById); //obtener un producto específico por su id.
+productRouter.get('/user/:userId', ProductController.getProductsByUserId); //Trae productos por id de usuario. Cuando se una solicitud get a esta ruta, se llama al método getProductByUserId del Product Controller, que maneja la lógica para obtener y devolver productos asociados con un suario específico.
+productRouter.post('/', ProductController.createProduct); //Crea un nuevo producto
+
+//Cada ruta está asociada a un método en el ProductController que contiene la lógica para manejar las solicitudes correspondientes. 
